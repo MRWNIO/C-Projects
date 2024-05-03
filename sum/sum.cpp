7cd7639475ccd36a3dd8a7fe71725143cc49d285
Marwan <93838171+MRWNIO@users.noreply.github.com>
2024-05-03 11:43:40 -0700

    Add files via upload

---
+++ sum/sum.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+{
+	int a,b,sum;
+	printf("Enter The First Number: ");
+	scanf("%d",&a);
+	printf("Enter Second Number: ");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("The Total= %d",sum);
+}
